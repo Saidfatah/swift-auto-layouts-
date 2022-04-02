818d5956d21fab1e275bf468b1b0cd7df117b8b0
SaidFatah <said_designer@outlook.com>
2022-04-02 14:37:32 +0000

    used StackViews for bottom controls

--- autoLayout/ViewController.swift
+++ autoLayout/ViewController.swift
@@ -7,6 +7,11 @@
 
 import UIKit
 
+
+extension UIColor{
+    static var mainPink = UIColor(red: 233/255, green: 68/255, blue: 133/255, alpha: 1)
+    static var mainPinkFainted = UIColor(red: 233/255, green: 68/255, blue: 133/255, alpha: 0.5)
+}
 class ViewController: UIViewController {
     
     let bearImageView:UIImageView = {
@@ -43,7 +48,33 @@ class ViewController: UIViewController {
         return textView
     }()
     
- 
+    private let previousButton : UIButton = {
+        let button = UIButton(type:.system)
+        button.setTitle("Previous", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.backgroundColor = .white
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+
+        button.setTitleColor(.gray, for: .focused)
+       return button
+    }()
+    private let nextButton : UIButton = {
+        let button = UIButton(type:.system)
+        button.setTitle("Next", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.backgroundColor = .white
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        button.setTitleColor(.mainPink, for: .normal)
+       return button
+    }()
+    private let pageControl:UIPageControl = {
+        let pc = UIPageControl()
+        pc.currentPage = 0
+        pc.numberOfPages = 4
+        pc.pageIndicatorTintColor = .mainPinkFainted
+        pc.currentPageIndicatorTintColor = .mainPink
+        return pc
+    }()
     
     // avoid polluting view load
     override func viewDidLoad() {
@@ -53,7 +84,27 @@ class ViewController: UIViewController {
         
         view.addSubview(descriptionTextView)
         setupLoayout()
-        
+        setupBottomControls()
+    }
+    
+    fileprivate func setupBottomControls(){
+        let bottomControlsStackView = UIStackView(arrangedSubviews: [
+           previousButton,
+           pageControl,
+           nextButton
+        ])
+        bottomControlsStackView.distribution = .fillEqually
+        bottomControlsStackView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(bottomControlsStackView)
+        NSLayoutConstraint.activate(
+            [
+                bottomControlsStackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+                bottomControlsStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+                bottomControlsStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+                bottomControlsStackView.heightAnchor.constraint(equalToConstant: 50),
+                bottomControlsStackView.widthAnchor.constraint(equalToConstant: 50)
+            ]
+        )
     }
     
     private func setupLoayout() {
